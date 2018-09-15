05d423032c2e3a420ebfcd237ae55a5909261bef
Ever <evercm95@hotmail.com>
2018-09-14 20:06:43 -0500

    mongoose connection

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var BankRouter = require('./routes/bank');
 
 var app = express();
 
-mongoose.connect('mongodb://root:bdatos@ds243295.mlab.com:43295/sirecaa-platform');
+mongoose.connect('mongodb://bankUser:Accenturetest0@ds111113.mlab.com:11113/bank');
 //Conect
 
 
--- routes/bank.js
+++ routes/bank.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var bankFuntions = require('/functions/bank');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/createCustomer', function(req, res, next) {
+    bankFuntions.createCustomer(req, res, next)
+});
+
+router.get('/validateId', function(req, res, next) {
+    bankFuntions.validateId(req, res, next)
+});
+
+router.get('/validatebirthdate', function(req, res, next) {
+    bankFuntions.validatebirthdate(req, res, next)
+});
+
+router.get('/validateEntryDate', function(req, res, next) {
+    bankFuntions.validateEntryDate(req, res, next)
+});
+
+router.get('/loan', function(req, res, next) {
+    bankFuntions.loan(req, res, next)
 });
 
 module.exports = router;
