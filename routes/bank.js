ae867c468b59f25c0d8e5e47ab7806de15716e58
Ever <evercm95@hotmail.com>
2018-09-21 20:24:54 -0500

    routes modification

--- routes/bank.js
+++ routes/bank.js
@@ -7,19 +7,19 @@ router.post('/createCustomer', function(req, res, next) {
     bankFuntions.createCustomer(req, res, next)
 });
 
-router.get('/validateId', function(req, res, next) {
+router.post('/validateId', function(req, res, next) {
     bankFuntions.validateId(req, res, next)
 });
 
-router.get('/validatebirthdate', function(req, res, next) {
+router.post('/validatebirthdate', function(req, res, next) {
     bankFuntions.validatebirthdate(req, res, next)
 });
 
-router.get('/validateEntryDate', function(req, res, next) {
+router.post('/validateEntryDate', function(req, res, next) {
     bankFuntions.validateEntryDate(req, res, next)
 });
 
-router.get('/loan', function(req, res, next) {
+router.post('/loan', function(req, res, next) {
     bankFuntions.loan(req, res, next)
 });
 
